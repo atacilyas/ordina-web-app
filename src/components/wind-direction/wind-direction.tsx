ca83d2f1ae8dcd9629aa0223c141550a9e48e5e3
ilyasAtac <ilyasatac@agurotech.com>
2023-02-02 23:57:51 +0100

    Added more translations

--- src/components/wind-direction/wind-direction.tsx
+++ src/components/wind-direction/wind-direction.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import "./wind-direction.css";
+import t from '../../i18n/i18n';
 
 export type WindDirectionProps = {
     degrees : number;
@@ -10,10 +11,10 @@ export class WindDirection extends React.Component<WindDirectionProps> {
         let degreesRotated = -45-90+this.props.degrees;
         let styleString = "rotate(" + degreesRotated.toString() + "deg)";
         return <div className='wind-direction-border'>
-             <p className='north'>N</p>
-             <p className='east'>E</p>
-             <p className='south'>S</p>
-             <p className='west'>W</p>
+             <p className='north'>{t('north-short')}</p>
+             <p className='east'>{t('east-short')}</p>
+             <p className='south'>{t('south-short')}</p>
+             <p className='west'>{t('west-short')}</p>
              <i className="arrow right" style={{transform: styleString}}></i>
         </div>;
     }
